05733f21e7d46abd4f3b6f21aff9369c39162612
Alvaro <alvieboy@alvie.com>
2010-11-04 16:36:05 +0000

    delay: fix overrun

--- hardware/zpuino/cores/zpuino/delay.cpp
+++ hardware/zpuino/cores/zpuino/delay.cpp
@@ -2,7 +2,13 @@
 
 void delayCycles(unsigned int cycles)
 {
-	unsigned int next = TIMERTSC + cycles;
+	unsigned int current = TIMERTSC;
+	unsigned int next = current + cycles;
+	/* This might overflow. We assume cycles is less than 2^30 */
+	if (next<current) {
+		while (TIMERTSC&0x80000000) {}    // Wait for timer to roll over
+	}
+
 	while (TIMERTSC<next) {}
 }
 
