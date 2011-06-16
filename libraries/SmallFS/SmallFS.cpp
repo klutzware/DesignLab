45f1aa9f9bcb39f896168990a3c45e82b74c5f40
Alvaro <alvieboy@alvie.com>
2011-06-16 08:55:17 +0100

    Add SmallFS library

---
+++ libraries/SmallFS/SmallFS.cpp
@@ -0,0 +1,228 @@
+#include "SmallFS.h"
+#include <string.h>
+#ifdef __linux__
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <endian.h>
+#include <stdio.h>
+
+#define BE32(x) be32toh(x)
+
+#else
+
+#include "WProgram.h"
+
+#define BE32(x) x
+
+#endif
+
+#undef SMALLFSDEBUG
+
+int SmallFS_class::begin()
+{
+
+#ifdef __linux__
+	fsstart = 0;
+	fd = ::open("smallfs.dat",O_RDONLY);
+	if (fd<0) {
+		perror("Cannot open smallfs.dat");
+		return -1;
+	}
+#else
+	struct boot_t *b = (struct boot_t*)bootloaderdata;
+	fsstart = b->spiend;
+
+#endif
+
+	spi_enable();
+
+	startread(fsstart);
+
+	read(&hdr,sizeof(hdr));
+
+	spi_disable();
+
+#ifdef SMALLFSDEBUG
+
+	unsigned debug;
+
+	Serial.print("Read magic ");
+	Serial.println(hdr.magic);
+	Serial.print("SPI end ");
+	Serial.println(fsstart);
+
+	Serial.print("Bdata at ");
+	Serial.println((unsigned)b);
+	// Test read
+
+	spi_enable();
+	startread(fsstart - 8);
+	read(&debug,sizeof(debug));
+	Serial.print("DD1 ");
+	Serial.println(debug);
+	read(&debug,sizeof(debug));
+	Serial.print("DD2 ");
+	Serial.println(debug);
+	read(&debug,sizeof(debug));
+	Serial.print("DD3 ");
+	Serial.println(debug);
+
+
+	spi_disable();
+#endif
+
+	if(BE32(hdr.magic) == SMALLFS_MAGIC)
+		return 0;
+	return -1;
+}
+
+
+void SmallFS_class::read(void *target, unsigned size)
+{
+#ifdef __linux__
+	if (fd>=0) {
+		::read(fd,target,size);
+	}
+#else
+	unsigned char *p=(unsigned char*)target;
+	while (size--) {
+		spiwrite(0);
+		*p++=spiread();
+	}
+#endif
+}
+
+void SmallFS_class::startread_i(unsigned address)
+{
+#ifdef __linux__
+	if (fd>=0)
+		::lseek(fd,address,SEEK_SET);
+#else
+	spiwrite(0x0B);
+    /*
+	spiwrite( ((unsigned char*)address)[2] );
+	spiwrite( ((unsigned char*)address)[1] );
+	spiwrite( ((unsigned char*)address)[0] );
+	*/
+#ifdef SMALLFSDEBUG
+	Serial.print("Seeking to ");
+	Serial.println(address);
+#endif
+	spiwrite(address>>16);
+	spiwrite(address>>8);
+	spiwrite(address);
+	spiwrite(0);
+#endif
+}
+
+
+SmallFSFile SmallFS_class::open(const char *name)
+{
+	/* Start at root offset */
+	unsigned o = fsstart + sizeof(struct smallfs_header);
+	unsigned char buf[256];
+	struct smallfs_entry e;
+
+	int c;
+
+	spi_enable();
+	startread(o);
+
+	for (c=BE32(hdr.numfiles); c; c--) {
+
+		read(&e,sizeof(struct smallfs_entry));
+
+		read(buf,e.namesize);
+		buf[e.namesize] = '\0';
+		/* Compare */
+		if (strcmp((const char*)buf,name)==0) {
+			spi_disable();
+
+			return SmallFSFile(BE32(e.offset) + fsstart, BE32(e.size));
+		}
+	}
+	spi_disable();
+
+	return SmallFSFile();
+}
+
+int SmallFSFile::read(void *buf, int s)
+{
+	if (!valid())
+		return -1;
+
+	if (seekpos==filesize)
+		return 0; /* EOF */
+
+	if (s + seekpos > filesize) {
+		s = filesize-seekpos;
+	}
+	SmallFS.spi_enable();
+
+	SmallFS.startread( seekpos + flashoffset );
+
+	SmallFS.read(buf,s);
+
+	SmallFS.spi_disable();
+	seekpos+=s;
+	return s;
+}
+
+int SmallFSFile::readCallback(int s, void (*callback)(unsigned char, void*), void *data)
+{
+	unsigned char c;
+
+	if (!valid())
+		return -1;
+
+	if (seekpos==filesize)
+		return 0; /* EOF */
+
+	if (s + seekpos > filesize) {
+		s = filesize-seekpos;
+	}
+#ifdef SMALLFSDEBUG
+	Serial.print("Will read ");
+	Serial.print(s);
+	Serial.print(" bytes from file at offset ");
+	Serial.print(flashoffset);
+	Serial.print(" seekpos is ");
+
+	Serial.println(seekpos);
+#endif
+	SmallFS.spi_enable();
+
+	SmallFS.startread( seekpos + flashoffset );
+
+	while (s--) {
+		SmallFS.read(&c,1);
+		callback(c,data);
+	}
+
+	SmallFS.spi_disable();
+	seekpos+=s;
+	return s;
+}
+
+void SmallFSFile::seek(int pos, int whence)
+{
+	int newpos;
+
+	if (whence==SEEK_SET)
+		newpos = pos;
+	else if (whence==SEEK_CUR)
+		newpos = seekpos + pos;
+	else
+		newpos = filesize + pos;
+
+	if (newpos>filesize)
+		newpos=filesize;
+
+	if (newpos<0)
+		newpos=0;
+
+	seekpos=newpos;
+}
+
+SmallFS_class SmallFS;
