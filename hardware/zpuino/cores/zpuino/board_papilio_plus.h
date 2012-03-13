264032eb3c3e000d7a0e12de9c676894ad62afd1
Alvaro <alvieboy@alvie.com>
2012-03-13 09:25:26 +0000

    vga: add missing VGALiquidCrystal boards: update boards

--- hardware/zpuino/cores/zpuino/board_papilio_plus.h
+++ hardware/zpuino/cores/zpuino/board_papilio_plus.h
@@ -11,7 +11,7 @@
 #error Undefined board memory size
 #endif
 
-#define BOARD_SPI_DIVIDER BIT(SPICP1)
+#define BOARD_SPI_DIVIDER BIT(SPICP0)
 
 #define IOBASE 0x08000000
 #define IO_SLOT_OFFSET_BIT 23
@@ -72,20 +72,9 @@
 #define FPGA_PIN_P17 47
 
 // Other pins
-#define FPGA_PIN_P88 48
-#define FPGA_PIN_P44 49
-#define FPGA_PIN_P90 50
-#define FPGA_PIN_P50 51
-#define FPGA_PIN_P24 52
-#define FPGA_PIN_P27 53
+#define FPGA_PIN_P24 48
+#define FPGA_LED_PIN 49
 
-#define FPGA_LED_PIN 54
-
-#define FPGA_PIN_UART_RX     FPGA_PIN_P88
-#define FPGA_PIN_UART_TX     FPGA_PIN_P90
-#define FPGA_PIN_SPI_MISO    FPGA_PIN_P44
-#define FPGA_PIN_SPI_MOSI    FPGA_PIN_P27
-#define FPGA_PIN_SPI_SCK     FPGA_PIN_P50
 #define FPGA_PIN_FLASHCS     FPGA_PIN_P24
 
 #define SPI_FLASH_SEL_PIN FPGA_PIN_FLASHCS
