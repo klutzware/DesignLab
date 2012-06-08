21fbd4f92ae822d9e95e30f38e5de2440902e713
Alvaro <alvieboy@alvie.com>
2012-06-08 09:16:12 +0100

    Missing files

---
+++ hardware/zpuino/cores/zpuino/new.cpp
@@ -0,0 +1,36 @@
+#ifndef ZPU
+
+#include <new.h>
+
+void * operator new(size_t size)
+{
+  return malloc(size);
+}
+
+void operator delete(void * ptr)
+{
+  free(ptr);
+} 
+
+int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
+void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
+void __cxa_guard_abort (__guard *) {}; 
+
+void __cxa_pure_virtual(void) {};
+#else
+#include <inttypes.h>
+
+void operator delete(void * ptr)
+{
+}
+
+extern "C" {
+void * malloc(int)
+{
+	return (void*)0;
+}
+void free(void*)
+{
+}
+};
+#endif
