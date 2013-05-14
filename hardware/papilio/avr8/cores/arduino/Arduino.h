f79b140343ea50fa4107f16b3d99388aee037ee1
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-05-14 09:39:17 -0600

    Cleanup unnecessary files from AVR8 core

--- hardware/papilio/avr8/cores/arduino/Arduino.h
+++ hardware/papilio/avr8/cores/arduino/Arduino.h
@@ -197,9 +197,9 @@ uint16_t makeWord(byte h, byte l);
 
 #define word(...) makeWord(__VA_ARGS__)
 
-unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
 
-void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0);
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration);
 void noTone(uint8_t _pin);
 
 // WMath prototypes
