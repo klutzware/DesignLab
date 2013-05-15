c3ea39cbc4537c08e99eef7ea98aaa4f4d93db38
Alvaro <alvieboy@alvie.com>
2013-05-15 15:03:54 +0100

    Interrupts: use optional argument for interrupts

--- hardware/zpuino/zpu20/cores/zpuino/zpuino.cpp
+++ hardware/zpuino/zpu20/cores/zpuino/zpuino.cpp
@@ -5,31 +5,40 @@
 
 #define ZPUINO_MAX_INTERRUPTS 16
 
-typedef void(*interrupt_type_t)(void);
+struct interrupt_type_t {
+    void(*func)(void*);
+    void*arg;
+};
 
 static interrupt_type_t itable[ZPUINO_MAX_INTERRUPTS]={0};
 
+int attachInterrupt(unsigned int line, void (*function)(void*), void *arg)
+{
+    if (line>=ZPUINO_MAX_INTERRUPTS)
+        return -1;
+    // cli();
+    itable[line].func = function;
+    itable[line].arg = arg;
+    // sei();
+    return 0;
+}
+
 void attachInterrupt(unsigned int line, void (*function)(void), int mode)
 {
-	if (line>=ZPUINO_MAX_INTERRUPTS)
-		return;
-	// cli();
-	itable[line] = function;
-	// sei();
+    attachInterrupt(line, (void(*)(void*))function, NULL);
 }
+
 void detachInterrupt(unsigned int line)
 {
-	// cli();
-	if (line>=ZPUINO_MAX_INTERRUPTS)
-		return;
-	itable[line] = 0;
-	// sei();
+    if (line>=ZPUINO_MAX_INTERRUPTS)
+        return;
+    itable[line].func = 0;
 }
 
 void _zpu_interrupt(unsigned int line)
 {
-	if (line>=ZPUINO_MAX_INTERRUPTS)
-		return;
-	if (itable[line])
-		itable[line]();
+    if (line>=ZPUINO_MAX_INTERRUPTS)
+        return;
+    if (itable[line].func)
+        itable[line].func(itable[line].arg);
 }
