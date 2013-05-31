1e9bcbf3cbd57ba5363509f48a12054aab1b9c13
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-05-31 14:52:32 -0600

    Zetris example update

--- hardware/zpuino/zpu/libraries/VGA/examples/zetris/zetris.ino
+++ hardware/zpuino/zpu/libraries/VGA/examples/zetris/zetris.ino
@@ -1,3 +1,25 @@
+/*
+  ZPUino
+  Papilio VGA Zetris Example
+  
+ Zetris Example:
+http://arcade.gadgetfactory.net/index.php?n=Main.Zetris 
+  
+ VGA Wing Introduction:
+ http://papilio.cc/index.php?n=Papilio.VGAWingIntroduction
+ 
+ Hardware:
+ If using an Arcade MegaWing or a LogicStart MegaWing then select the appropriate "Hyperion" board type in the ZAP IDE.
+ If using a VGA Wing or some other device then use the "Hyperion" pinselect board type in the ZAP IDE and be sure to uncomment "setup_pin_select" in setup.
+ 
+ ZPUino Variant:
+ Hyperion "The Lord of Light" with HQVGA adapter is needed for this example sketch
+
+ created 2012
+ by Alvaro Lopes
+
+ */
+
 #include "VGA.h"
 #include "SmallFS.h"
 #include "structures.h"
