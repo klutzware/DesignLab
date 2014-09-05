00f6df60b89c10682767fa4775cdd1c4b345f36d
jackgassett <jack.gassett@gadgetfactory.net>
2014-09-04 17:45:00 -0700

    Changes for Papilio DUO VID/PID

--- hardware/arduino/avr/bootloaders/caterina/Descriptors.c
+++ hardware/arduino/avr/bootloaders/caterina/Descriptors.c
@@ -199,6 +199,8 @@ const USB_Descriptor_String_t ProductString =
 	.UnicodeString			= L"Arduino Micro   "
 	#elif DEVICE_PID == 0x003C
 	.UnicodeString			= L"Arduino Esplora "
+	#elif DEVICE_PID == 0x60a4
+	.UnicodeString			= L"Papilio DUO     "	
 	#else
 	.UnicodeString			= L"USB IO board    "
 	#endif
