2e8e579cedbd14c09013ec08e58bb6f0370c1cdb
jackgassett <jack.gassett@gadgetfactory.net>
2014-10-23 15:09:27 -0600

    Cleanup TOC

--- build/shared/examples/00.Papilio_Schematic_Library/examples/TOC.ino
+++ build/shared/examples/00.Papilio_Schematic_Library/examples/TOC.ino
@@ -34,13 +34,13 @@ Or visit the Papilio Forums.
 http://forum.gadgetfactory.net
 
 ZPUINO SOFT PROCESSOR PROJECTS
-	Audio Project - Playing Amiga Mod Files
+    Audio Project - Playing Amiga Mod Files
 	Relive the glory days of the Amiga by playing those 16-bit Amiga Mod files on your Papilio. 
 	This project uses a ZPUino Soft Processor with a wishbone audio passthrough hardware device connected to a delta-sigma DAC.
 	There is a port of a small Amiga Mod player that runs on the ZPUino soft processor and generates audio data that is sent through the wishbone bus to the audio passthrough hardware.
 	example://Audio_ModFile_simple/Audio_ModFile_simple.ino
 
-	Audio Project - Commodore 64 SID
+    Audio Project - Commodore 64 SID
 	Play those old Commodore 64 SID files or make a MIDI instrument with the Commodore 64 SID chip. 
 	The VHDL model of the Commodore SID chip was created from a famous interview with Bob Yannes where he detailed the digital logic that was used to create the SID chip. 
 	VHDL hackers were quick to jump on this information and recreate the SID chip with the exact same details of the original chip. 
@@ -49,36 +49,36 @@ ZPUINO SOFT PROCESSOR PROJECTS
 	No VHDL is required to use this chip though, its all schematic goodness. Just drop the chip onto a wishbone bus and use the included Arduino style libraries to drive it. This project shows you how.
 	example://Audio_SID_simple/Audio_SID_simple.ino
 	
-	Audio Project - YM2149 Audio Chip
+    Audio Project - YM2149 Audio Chip
 	Much like the Commodore project but with the YM2149 audio chip used in the Atari ST. There are tons of ym files out there and this chip and included libraries lets you play them all!
 	example://Audio_YM2149_simple/Audio_YM2149_simple.ino
 	
-	Audio Project - RetroCade Synth
+    Audio Project - RetroCade Synth
 	How about putting all of these audio chips together and putting them under MIDI control? That's what this project does, there is even a special MegaWing to provide high quality audio jacks, MIDI connnectors, and a Digital readout.
 	example://Audio_RetroCade_Synth/Audio_RetroCade_Synth.ino
 	
-	Hardware Project - LogicStart MegaWing 
+    Hardware Project - LogicStart MegaWing 
 	This is a demo project that shows you how to use draw a circuit that uses all of the LogicStart MegaWing components.
 	example://MegaWing_Logicstart/MegaWing_Logicstart.ino
 	
-	Hardware Project - Analog Wing 
+    Hardware Project - Analog Wing 
 	This is a demo project that shows you how to use draw a circuit to use the SPI ADC Analog Wing.
 	example://WING_Analog/WING_Analog.ino	
 	
-	Hardware Project - VGA8 Wing 
+    Hardware Project - VGA8 Wing 
 	This is a demo project that shows you how to use draw a circuit to use the 8 color VGA Wing.
 	example://Wing_VGA8/Wing_VGA8.ino	
 
 BENCHY DEBUGGING PROJECTS
-	Waveform Generator
+    Waveform Generator
 	Generate sin, cos, sawtooth, and square waves and control the frequency using a sketch.
 	example://Benchy_Waveform_Generator/Benchy_Waveform_Generator.ino
 	
-	Stand Alone Logic Analyzer
+    Stand Alone Logic Analyzer
 	This is the "Sump" Logic Analyzer that is used in the Open Bench Logic Sniffer. Use this to make a standalone logic analyzer that is directly connected to the Papilio serial port. Sample at speeds up to 200Mhz!
 	example://Benchy_Sump_LogicAnalyzer/Benchy_Sump_LogicAnalyzer.ino
 
-	JTAG Logic Analyzer
+    JTAG Logic Analyzer
 	This is the "Sump" Logic Analyzer that is used in the Open Bench Logic Sniffer. This example uses the JTAG channel to communicate with your computer. 
 	This allows you to debug your circuits without using the precious serial port. 
 	Drop this into your circuit designs and probe your signals without impacting your design. Sample at speeds up to 200Mhz!
