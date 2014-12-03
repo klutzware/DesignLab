197ea2be6ad1f8d1197bcb25f308d93e8a328787
jackgassett <jack.gassett@gadgetfactory.net>
2014-12-03 14:13:44 -0800

    Give permissions for Papilio DUO

--- hardware/tools/papilio/papilio_loader/ftdi_user.sh
+++ hardware/tools/papilio/papilio_loader/ftdi_user.sh
@@ -2,4 +2,5 @@
 sudo usermod -a -G dialout `whoami`
 sudo chgrp dialout hardware/tools/papilio/papilio_loader/papilio-prog
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", GROUP="dialout"' > /etc/udev/rules.d/papilio.rules
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="7bc0", GROUP="dialout"' > /etc/udev/rules.d/papilio.rules
 
